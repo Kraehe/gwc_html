dc1d56c41dc50a48b2be7c9a9d72438cca273cf3
Giovanna Rodriguez <gigi.giggles01@gmail.com>
2019-07-24 11:22:19 -0400

    added wizpage content

---
+++ wizardpage/script.js
@@ -0,0 +1,12 @@
+function wizvis() {
+  content = document.getElementById("wiz");
+  content.innerHTML = "KEEP YOUR FILTHY PAWS OFF OF ME! RELEASE ME!!";
+}
+function wizdis() {
+  content = document.getElementById("wiz");
+  content.innerHTML = "";
+}
+function oof() {
+  var ouch = document.getElementById("wizoof");
+  wizoof.play();
+}
